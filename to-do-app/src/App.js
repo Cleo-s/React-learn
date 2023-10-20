dfc23fd1d99611829b47b0163c54f12ee2264639
Cleo-s <cleo.mics@gmail.com>
2023-10-20 22:59:41 +0200

    add: README.md file

---
+++ to-do-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Displayer from './components/Displayer';
+
+function App() {
+  return (
+    <div className="App">
+      <Displayer />
+    </div>
+  );
+}
+
+export default App; 
