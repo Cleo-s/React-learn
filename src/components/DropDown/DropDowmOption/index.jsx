bbc3950222fb5cc551444acc9686916352486393
ZakharovVladyslav <vladislavozakharov@gmail.com>
2024-04-01 20:34:19 +0300

    upd: dropdown; add: comments for fixes

--- src/components/DropDown/DropDowmOption/index.jsx
+++ src/components/DropDown/DropDowmOption/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './dropDownOption.css';
+import "./dropDownOption.css";
 
-function DropDownOption({ key, label, status, onClick, className }) {
-
-    return (
-        <button className={className} key={key} status={status} onClick={onClick}>{label}</button>
-    )
+function DropDownOption({ key, label, onClick, className }) {
+   return (
+      // TODO change button tag to button component
+      <button className={className} key={key} onClick={onClick}>
+         {label}
+      </button>
+   );
 }
 
 export default DropDownOption;
--- src/components/DropDown/index.jsx
+++ src/components/DropDown/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 
-
 import DropDownOption from "./DropDowmOption";
 import StatusUpdate from "../statusUpdate";
 import { IconedButton } from "../IconedButton";
@@ -11,81 +10,58 @@ import ChevronDownIcon from "../Icons/ChevronDownIcon/index";
 import "./DropDown.css";
 
 function DropDown({ openedDropDown, setOpenedDropDown, tasks, task }) {
+   const dropDownButtonsOptions = [
+      {
+         label: "Pending",
+         status: Status.pending,
+         className: "pending-dropdown-button",
+      },
+      {
+         label: "Working",
+         status: Status.working,
+         className: "working-dropdown-button",
+      },
+      { label: "Done", status: Status.done, className: "done-dropdown-button" },
+      {
+         label: "Cancelled",
+         status: Status.cancelled,
+         className: "cancelled-dropdown-button",
+      },
+   ];
 
-  const dropDownButtonsOptions = [
-  {label: "Pending", status: Status.pending, className: "pending-dropdown-button"}, 
-  {label: "Working", status: Status.working, className: "working-dropdown-button"}, 
-  {label: "Done", status: Status.done, className: "done-dropdown-button"}, 
-  {label: "Cancelled", status: Status.cancelled, className: "cancelled-dropdown-button"}
-]
+   const handleDropDown = () => {
+      setOpenedDropDown(!openedDropDown);
+   };
 
-  const handleDropDown = () => {
-    setOpenedDropDown(!openedDropDown);
-  };
+   const handleButtonStatusChange = (currentStatus) => {
+      const targetTask = tasks.find((taskObj) => taskObj.id === task.id);
+      targetTask.status = currentStatus;
+      setOpenedDropDown(false);
+   };
 
-  const handleButtonStatusChange = (currentStatus) => {
-    const targetTask = tasks.find((taskObj) => taskObj.id === task.id);
-    targetTask.status = currentStatus;
-    setOpenedDropDown(false);
-  };
+   return (
+      <div className="dropdown-div">
+         <IconedButton
+            icon={<ChevronDownIcon />}
+            onClick={handleDropDown}
+            classname={"dropdown-button"}
+         />
+         {task.status && <StatusUpdate status={task.status} />}
 
-  return (
-    <div className="dropdown-div">
-      <IconedButton
-        icon={<ChevronDownIcon />}
-        onClick={handleDropDown}
-        classname={"dropdown-button"}
-      />
-      {task.status && <StatusUpdate status={task.status} />}
-
-      {openedDropDown ? (
-        <section className="dropdown-section">
-          <ul className="dropdown-table">
-            {dropDownButtonsOptions.map((option, index) => {
-              return (
-                <DropDownOption
-                  key={index}
-                  label={option.label}
-                  status={option.status}
-                  className={option.className}
-                  onClick={() => handleButtonStatusChange(option.status)}
-                />
-              );
-            })}
-            {/*
-            <li>
-              <Button
-                className="pending-dropdown-button"
-                label="Pending"
-                onClick={() => handleButtonStatusChange(Status.pending)}
-              />
-            </li>
-            <li>
-              <Button
-                className="working-dropdown-button"
-                label="Working"
-                onClick={() => handleButtonStatusChange(Status.working)}
-              />
-            </li>
-            <li>
-              <Button
-                className="done-dropdown-button"
-                label="Done"
-                onClick={() => handleButtonStatusChange(Status.done)}
-              />
-            </li>
-            <li>
-              <Button
-                className="cancelled-dropdown-button"
-                label="Cancelled"
-                onClick={() => handleButtonStatusChange(Status.cancelled)}
-              />
-      </li>*/}
-          </ul>
-        </section>
-      ) : null}
-    </div>
-  );
+         {openedDropDown && (
+            <ul className="dropdown-table">
+               {dropDownButtonsOptions.map((option, index) => (
+                  <DropDownOption
+                     key={index}
+                     label={option.status}
+                     className={option.className}
+                     onClick={() => handleButtonStatusChange(option.status)}
+                  />
+               ))}
+            </ul>
+         )}
+      </div>
+   );
 }
 
 export default DropDown;
